e4a01722096c9c794ae88aa35c25dd5aaa2c6d37
andrewshimel <andrewshimel@gmail.com>
2020-02-05 20:44:29 -0600

    Safety Commit

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/EmployerRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/EmployerRepository.java
@@ -3,6 +3,9 @@ package org.launchcode.javawebdevtechjobspersistent.models.data;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
+@Transactional
 public interface EmployerRepository extends CrudRepository {
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/SkillRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/SkillRepository.java
@@ -3,6 +3,10 @@ package org.launchcode.javawebdevtechjobspersistent.models.data;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+
 @Repository
+@Transactional
 public interface SkillRepository extends CrudRepository {
 }
