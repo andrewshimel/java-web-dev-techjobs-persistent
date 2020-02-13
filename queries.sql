1c0f69676d873f641bb37608a1d6930e18bb31cb
andrewshimel <andrewshimel@gmail.com>
2020-02-12 19:40:48 -0600

    Working with sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,12 @@
 ## Part 1: Test it with SQL
 SELECT COLUMN_NAME, DATA_TYPE FROM information_schema.Columns WHERE TABLE_NAME = "job"; 
 ## Part 2: Test it with SQL
-SELECT name FROM employer WHERE location = "St. Louis"
+SELECT name FROM employer WHERE location = "St. Louis";
 ## Part 3: Test it with SQL
-
+/*DROP TABLE hibernate_sequence;
+DROP TABLE job_skills;
+DROP TABLE job;
+DROP TABLE skill;
+DROP TABLE employer;*/
 ## Part 4: Test it with SQL
+SELECT name, description FROM skill INNER JOIN job_skills ON skill.id= job_skills.skills_id;
